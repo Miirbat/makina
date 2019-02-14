4b93414875014c4923921c37515d747bcbb151aa
miirbat <mirna.plakalovic@gmail.com>
2019-02-13 19:06:24 -0500

    initialize app

---
+++ src/Page.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './page.css'
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="grid">
+        <div className="one header">
+          <div className="innerwrapper">
+          </div>
+        </div>
+        <div className="two">
+          <div className="innerwrapper">
+          <img src="" alt="hello" height="" />
+          </div>
+        </div>
+        <div className="three">
+          <div className="innerwrapper">
+            <img src="" alt="hello" height="" />
+          </div>
+        </div>
+        <div className="four">
+          <div className="innerwrapper">
+            <img src="" alt="hello" height="" />
+          </div>
+        </div>
+        <div className="four">
+          <div className="innerwrapper">
+            <img src="" alt="hello" height="" />
+          </div>
+        </div>
+        <div className="five">
+          <div className="innerwrapper">
+          </div>
+        </div>
+        <div className="six">
+          <div className="innerwrapper">
+          </div>
+        </div>
+        <div className="seven">
+          <div className="innerwrapper">
+          </div>
+        </div>
+        <div className="eight">
+          <div className="innerwrapper">
+          </div>
+        </div>
+        <div className="nine">
+          <div className="innerwrapper">
+          </div>
+        </div>
+        <div className="ten footer">
+          <div className="innerwrapper">
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page;
